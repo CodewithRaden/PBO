7e01186c707bd597ff368784ec966857dff8b8c7
CodewithRaden <126065881+CodewithRaden@users.noreply.github.com>
2023-12-09 14:50:53 +0700

    copy minimal ganti

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy.sql import func
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+# db = SQLAlchemy(app)'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/web_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db = SQLAlchemy(app)
 
 class Radiant(db.Model):  
